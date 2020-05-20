7901eee11238fa3fb1d92690b889af7670bfef81
Šimon Lukašík <isimluk@fedoraproject.org>
2020-05-20 17:12:14 +0200

    Start parsing imported schemas (xsd depedencies)

--- pkg/xsd/schema.go
+++ pkg/xsd/schema.go
@@ -4,13 +4,14 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 // Schema is the root XSD element
 type Schema struct {
 	XMLName         xml.Name  `xml:"http://www.w3.org/2001/XMLSchema schema"`
 	TargetNamespace string    `xml:"targetNamespace,attr"`
-	Imports         []Import  `xml:import`
+	Imports         []Import  `xml:"import"`
 	Elements        []Element `xml:"element"`
 }
 
@@ -28,6 +29,13 @@ func Parse(xsdPath string) (*Schema, error) {
 		return nil, fmt.Errorf("Error decoding XSD: %s", err)
 	}
 
+	dir := filepath.Dir(xsdPath)
+	for _, imp := range schema.Imports {
+		if err := imp.Load(dir); err != nil {
+			return nil, err
+		}
+	}
+
 	return &schema, nil
 }
 
@@ -43,4 +51,10 @@ type Import struct {
 	XMLName        xml.Name `xml:"http://www.w3.org/2001/XMLSchema import"`
 	Namespace      string   `xml:"namespace,attr"`
 	SchemaLocation string   `xml:"schemaLocation,attr"`
+	importedSchema *Schema  `xml:"-"`
+}
+
+func (i *Import) Load(baseDir string) (err error) {
+	i.importedSchema, err = Parse(filepath.Join(baseDir, i.SchemaLocation))
+	return
 }
