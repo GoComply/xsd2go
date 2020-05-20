2abce5d45eb9ac1ab6aac420761009020c0562ed
Šimon Lukašík <isimluk@fedoraproject.org>
2020-05-20 21:19:10 +0200

    xccdf:Benchmark can have both 'Id' and 'id' attribute
    And hence our code has to account for that.

--- pkg/xsd/attribute.go
+++ pkg/xsd/attribute.go
@@ -2,19 +2,25 @@ package xsd
 
 import (
 	"encoding/xml"
+	"fmt"
 
 	"github.com/iancoleman/strcase"
 )
 
 // Attribute defines single XML attribute
 type Attribute struct {
-	XMLName xml.Name `xml:"http://www.w3.org/2001/XMLSchema attribute"`
-	Name    string   `xml:"name,attr"`
-	Type    string   `xml:"type,attr"`
-	Use     string   `xml:"use,attr"`
+	XMLName        xml.Name `xml:"http://www.w3.org/2001/XMLSchema attribute"`
+	Name           string   `xml:"name,attr"`
+	Type           string   `xml:"type,attr"`
+	Use            string   `xml:"use,attr"`
+	DuplicateCount uint     `xml:"-"`
 }
 
 // Public Go Name of this struct item
 func (a *Attribute) GoName() string {
-	return strcase.ToCamel(a.Name)
+	name := a.Name
+	if a.DuplicateCount >= 2 {
+		name = fmt.Sprintf("%s%d", name, a.DuplicateCount)
+	}
+	return strcase.ToCamel(name)
 }
--- pkg/xsd/element.go
+++ pkg/xsd/element.go
@@ -13,7 +13,21 @@ type Element struct {
 
 func (e *Element) Attributes() []Attribute {
 	if e.ComplexType != nil {
-		return e.ComplexType.Attributes
+		attributes := e.ComplexType.Attributes
+
+		// Handle improbable name clash. Consider XSD defining two attributes on the element:
+		// "id" and "Id", this would create name clash given the camelization we do.
+		goNames := map[string]uint{}
+		for idx, _ := range attributes {
+			attribute := &attributes[idx]
+			count := goNames[attribute.GoName()]
+			count += 1
+			goNames[attribute.GoName()] = count
+			attribute.DuplicateCount = count
+			// Second GoName may be different depending on the DuplicateCount
+			goNames[attribute.GoName()] = count
+		}
+		return attributes
 	}
 	return []Attribute{}
 }
