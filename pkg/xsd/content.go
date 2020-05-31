fdc8dc871fae278d75fe4be861ee9f945b35599c
Šimon Lukašík <isimluk@fedoraproject.org>
2020-05-31 19:23:28 +0200

    Refactor: move simpleContent to a separate file

---
+++ pkg/xsd/content.go
@@ -0,0 +1,23 @@
+package xsd
+
+import (
+	"encoding/xml"
+)
+
+type SimpleContent struct {
+	XMLName   xml.Name   `xml:"http://www.w3.org/2001/XMLSchema simpleContent"`
+	Extension *Extension `xml:"extension"`
+}
+
+func (sc *SimpleContent) Attributes() []Attribute {
+	if sc.Extension != nil {
+		return sc.Extension.Attributes
+	}
+	return []Attribute{}
+}
+
+type Extension struct {
+	XMLName    xml.Name    `xml:"http://www.w3.org/2001/XMLSchema extension"`
+	Base       string      `xml:"base,attr"`
+	Attributes []Attribute `xml:"attribute"`
+}
--- pkg/xsd/types.go
+++ pkg/xsd/types.go
@@ -76,24 +76,6 @@ func (ct *ComplexType) compile(sch *Schema) {
 	}
 }
 
-type SimpleContent struct {
-	XMLName   xml.Name   `xml:"http://www.w3.org/2001/XMLSchema simpleContent"`
-	Extension *Extension `xml:"extension"`
-}
-
-func (sc *SimpleContent) Attributes() []Attribute {
-	if sc.Extension != nil {
-		return sc.Extension.Attributes
-	}
-	return []Attribute{}
-}
-
-type Extension struct {
-	XMLName    xml.Name    `xml:"http://www.w3.org/2001/XMLSchema extension"`
-	Base       string      `xml:"base,attr"`
-	Attributes []Attribute `xml:"attribute"`
-}
-
 type SimpleType struct {
 	XMLName xml.Name `xml:"http://www.w3.org/2001/XMLSchema simpleType"`
 	Name    string   `xml:"name,attr"`
