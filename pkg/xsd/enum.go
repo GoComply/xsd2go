78771a50dfbe5093061329fc4f84fb61eb6c4675
Guy Rozendorn <guy@rzn.co.il>
2020-10-31 23:59:53 +0200

    forgot pkg/xsd/enum.go

---
+++ pkg/xsd/enum.go
@@ -0,0 +1,29 @@
+package xsd
+
+import (
+	"encoding/xml"
+
+	"github.com/iancoleman/strcase"
+)
+
+// Attribute defines single XML attribute
+type Enumeration struct {
+	XMLName xml.Name `xml:"http://www.w3.org/2001/XMLSchema enumeration"`
+	Value   string   `xml:"value,attr"`
+}
+
+// Public Go Name of this struct item
+func (e *Enumeration) GoName() string {
+	return strcase.ToCamel(e.Value)
+}
+
+func (e *Enumeration) Modifiers() string {
+	return "-"
+}
+
+func (e *Enumeration) XmlName() string {
+	return e.Value
+}
+
+func (e *Enumeration) compile(s *Schema) {
+}
