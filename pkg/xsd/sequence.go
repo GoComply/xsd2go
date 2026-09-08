6e6e1f933c1a872099f3b450dc25c5734270be47
Henry Avila <henry.avila@qbt.ch>
2026-09-08 12:34:06 +0200

    fix: propagate minOccurs/maxOccurs cardinality from sequence elements to children
    Previously, Sequence and SequenceAll structs did not parse or propagate
    minOccurs/maxOccurs attributes to child elements. This caused schemas using
    <xs:sequence minOccurs="0" maxOccurs="21"> with elements inside to be
    ignored entirely by the code generator.
    
    Changes:
    - Added MinOccurs and MaxOccurs fields to Sequence and SequenceAll structs
    - Updated compile() methods to propagate cardinality to child elements
    - Added support for nested sequences inheriting parent cardinality
    - Fields inside sequences with maxOccurs > 1 now correctly become slices
    - Fields with minOccurs="0" get proper omitempty handling
    
    Example fix:
      <xs:sequence minOccurs="0" maxOccurs="21">
        <xs:element name="Item"/>
      </xs:sequence>
      Now generates: Item []Item `xml:"Item,omitempty"`
    
    Updated xmldsig-core-schema.xsd.out to reflect correct output for
    SPKIDataType which has <sequence maxOccurs="unbounded">
    
    Fixes issue where sequences containing minOccurs/maxOccurs were completely ignored

--- pkg/xsd/sequence.go
+++ pkg/xsd/sequence.go
@@ -5,9 +5,12 @@ import (
 )
 
 type Sequence struct {
-	XMLName     xml.Name  `xml:"http://www.w3.org/2001/XMLSchema sequence"`
-	ElementList []Element `xml:"element"`
-	Choices     []Choice  `xml:"choice"`
+	XMLName     xml.Name   `xml:"http://www.w3.org/2001/XMLSchema sequence"`
+	MinOccurs   string     `xml:"minOccurs,attr"`
+	MaxOccurs   string     `xml:"maxOccurs,attr"`
+	ElementList []Element  `xml:"element"`
+	Choices     []Choice   `xml:"choice"`
+	Sequences   []Sequence `xml:"sequence"`
 	allElements []Element
 }
 
@@ -19,6 +22,14 @@ func (s *Sequence) compile(sch *Schema, parentElement *Element) {
 	for idx := range s.ElementList {
 		el := &s.ElementList[idx]
 		el.compile(sch, parentElement)
+		if s.MaxOccurs == "unbounded" {
+			el.MaxOccurs = "unbounded"
+		} else if s.MaxOccurs != "" && el.MaxOccurs == "" {
+			el.MaxOccurs = s.MaxOccurs
+		}
+		if s.MinOccurs == "0" && el.MinOccurs == "" {
+			el.MinOccurs = "0"
+		}
 	}
 	s.allElements = s.ElementList
 
@@ -26,14 +37,47 @@ func (s *Sequence) compile(sch *Schema, parentElement *Element) {
 		c := &s.Choices[idx]
 		c.compile(sch, parentElement)
 
+		for _, nestedEl := range c.Elements() {
+			if s.MaxOccurs == "unbounded" {
+				nestedEl.MaxOccurs = "unbounded"
+			} else if s.MaxOccurs != "" && nestedEl.MaxOccurs == "" {
+				nestedEl.MaxOccurs = s.MaxOccurs
+			}
+			if s.MinOccurs == "0" && nestedEl.MinOccurs == "" {
+				nestedEl.MinOccurs = "0"
+			}
+		}
+
 		s.allElements = append(s.allElements, c.Elements()...)
+	}
+
+	for idx := range s.Sequences {
+		nestedSeq := &s.Sequences[idx]
+		nestedSeq.compile(sch, parentElement)
+
+		// Propagate cardinality from parent sequence to nested sequence's elements
+		for _, nestedEl := range nestedSeq.allElements {
+			if s.MaxOccurs == "unbounded" {
+				nestedEl.MaxOccurs = "unbounded"
+			} else if s.MaxOccurs != "" && nestedEl.MaxOccurs == "" {
+				nestedEl.MaxOccurs = s.MaxOccurs
+			}
+			if s.MinOccurs == "0" && nestedEl.MinOccurs == "" {
+				nestedEl.MinOccurs = "0"
+			}
+		}
+
+		s.allElements = append(s.allElements, nestedSeq.allElements...)
 	}
 }
 
 type SequenceAll struct {
-	XMLName     xml.Name  `xml:"http://www.w3.org/2001/XMLSchema all"`
-	ElementList []Element `xml:"element"`
-	Choices     []Choice  `xml:"choice"`
+	XMLName     xml.Name   `xml:"http://www.w3.org/2001/XMLSchema all"`
+	MinOccurs   string     `xml:"minOccurs,attr"`
+	MaxOccurs   string     `xml:"maxOccurs,attr"`
+	ElementList []Element  `xml:"element"`
+	Choices     []Choice   `xml:"choice"`
+	Sequences   []Sequence `xml:"sequence"`
 	allElements []Element
 }
 
@@ -45,6 +89,14 @@ func (s *SequenceAll) compile(sch *Schema, parentElement *Element) {
 	for idx := range s.ElementList {
 		el := &s.ElementList[idx]
 		el.compile(sch, parentElement)
+		if s.MaxOccurs == "unbounded" {
+			el.MaxOccurs = "unbounded"
+		} else if s.MaxOccurs != "" && el.MaxOccurs == "" {
+			el.MaxOccurs = s.MaxOccurs
+		}
+		if s.MinOccurs == "0" && el.MinOccurs == "" {
+			el.MinOccurs = "0"
+		}
 	}
 	s.allElements = s.ElementList
 
@@ -52,6 +104,36 @@ func (s *SequenceAll) compile(sch *Schema, parentElement *Element) {
 		c := &s.Choices[idx]
 		c.compile(sch, parentElement)
 
+		for _, nestedEl := range c.Elements() {
+			if s.MaxOccurs == "unbounded" {
+				nestedEl.MaxOccurs = "unbounded"
+			} else if s.MaxOccurs != "" && nestedEl.MaxOccurs == "" {
+				nestedEl.MaxOccurs = s.MaxOccurs
+			}
+			if s.MinOccurs == "0" && nestedEl.MinOccurs == "" {
+				nestedEl.MinOccurs = "0"
+			}
+		}
+
 		s.allElements = append(s.allElements, c.Elements()...)
+	}
+
+	for idx := range s.Sequences {
+		nestedSeq := &s.Sequences[idx]
+		nestedSeq.compile(sch, parentElement)
+
+		// Propagate cardinality from parent sequence to nested sequence's elements
+		for _, nestedEl := range nestedSeq.allElements {
+			if s.MaxOccurs == "unbounded" {
+				nestedEl.MaxOccurs = "unbounded"
+			} else if s.MaxOccurs != "" && nestedEl.MaxOccurs == "" {
+				nestedEl.MaxOccurs = s.MaxOccurs
+			}
+			if s.MinOccurs == "0" && nestedEl.MinOccurs == "" {
+				nestedEl.MinOccurs = "0"
+			}
+		}
+
+		s.allElements = append(s.allElements, nestedSeq.allElements...)
 	}
 }
